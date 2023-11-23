4c582abe75730495b2187a75356f467d69a07aed
luochao <1055120207@qq.com>
2023-11-23 18:50:25 +0800

    feat: 接入pv统计工具不蒜子

--- src/pages/module/index.tsx
+++ src/pages/module/index.tsx
@@ -1,7 +1,14 @@
+import { flexCenterOpts } from "../../core/style/utils";
+
 export default function Dashboard() {
+  const extraHeight = 48;
+
   return (
-    <div>
+    <div css={{ height: `calc(100% - ${extraHeight}px)`, paddingBottom: 24, position: "relative" }}>
       该面板功能正在研发中！
+      <div css={[flexCenterOpts(), { width: "100%", height: extraHeight, position: "absolute", bottom: "-72px" }]}>
+        <span id="busuanzi_container_site_pv">本站总访问量&nbsp;<span id="busuanzi_value_site_pv" />&nbsp;次</span>
+      </div>
     </div>
   );
 }
